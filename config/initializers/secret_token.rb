ca4e4e8ec7e51587489dddad16fe3968dd9e6e22
kimimaro <kimirius@gmail.com>
2015-10-31 23:36:55 +0800

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aboutme::Application.config.secret_key_base = 'bf575e93e3ba0703dab3885f7058bb3ab00f87dc3dea22fc1d02879276b4b3837e7f98d0d27a75390476b5e2fa2f4de902c9f9c773aaa67da632f20ea5998b08'
---
+++ config/unicorn.rb
@@ -0,0 +1,71 @@
+# Set environment to development unless something else is specified
+env = ENV["RAILS_ENV"] || "development"
+
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+worker_processes 4
+
+# app settings
+app_dir = File.expand_path("../..", __FILE__)
+app_name = "aboutme"
+pid_path = "#{app_dir}/tmp/pids/unicorn.#{app_name}.pid"
+sock_path = "#{app_dir}/tmp/sockets/unicorn.#{app_name}.sock"
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen sock_path, :backlog => 64
+
+# Preload our app for more speed
+preload_app true
+
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 30
+
+pid pid_path
+
+# Production specific settings
+if env == "production"
+  # Help ensure your application will always spawn in the symlinked
+  # "current" directory that Capistrano sets up.
+  
+  working_directory "#{app_dir}/current" # "/var/www/projects/current"
+
+  # feel free to point this anywhere accessible on the filesystem
+  user 'deploy', 'deploy'
+  shared_path = "#{app_dir}/current/shared"
+
+  stderr_path "#{shared_path}/log/unicorn.stderr.log"
+  stdout_path "#{shared_path}/log/unicorn.stdout.log"
+end
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "#{pid_path}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
